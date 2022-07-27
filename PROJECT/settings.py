cef42fc6d6732fc17e30efd2263271e41402fec2
prosper-20 <edwardprosper001@gmail.com>
2022-07-27 21:49:46 +0100

    updates

--- PROJECT/settings.py
+++ PROJECT/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'landing.apps.LandingConfig',
+    'social.apps.SocialConfig'
 ]
 
 MIDDLEWARE = [
