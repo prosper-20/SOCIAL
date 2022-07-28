17161aac7235ed70e1f29bb9695c8f5cf12db17e
prosper-20 <edwardprosper001@gmail.com>
2022-07-28 21:01:09 +0100

    Add-ons

--- social/urls.py
+++ social/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
 from social.models import Post
-from .views import PostListView
+from .views import PostListView, PostDetailView
 
 urlpatterns = [
-    path("", PostListView.as_view(), name="post-list")
+    path("", PostListView.as_view(), name="post-list"),
+    path("post/<int:pk>/", PostDetailView.as_view(), name="post-detail")
 ]
