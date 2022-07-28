b66136191ce663b5dc4302bbb256de4be392241d
prosper-20 <edwardprosper001@gmail.com>
2022-07-28 21:41:42 +0100

    Add-ons

--- social/admin.py
+++ social/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
 # Register your models here.
 
@@ -7,3 +7,11 @@ class PostAdmin(admin.ModelAdmin):
     list_display = ["created_on", "author"]
 
 admin.site.register(Post, PostAdmin)
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ["comment", "author", "post"]
+
+
+admin.site.register(Comment, CommentAdmin)
+
