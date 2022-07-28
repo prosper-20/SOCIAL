f974b4ae920f981c2ff537e708becd5789da5570
prosper-20 <edwardprosper001@gmail.com>
2022-07-28 16:47:47 +0100

    updates

--- social/views.py
+++ social/views.py
@@ -1,14 +1,18 @@
+import imp
 from django.shortcuts import render
 from django.views import View
 from .models import Post
+from .forms import PostForm
 
 # Create your views here.
 
 class PostListView(View):
     def get(self, request, *args, **kwargs):
         posts = Post.objects.all().order_by('-created_on')
+        form = PostForm()
 
         context = {
-            'posts': posts
+            'posts': posts,
+            'form': form
         }
         return render(request, 'social/post_list.html', context)
