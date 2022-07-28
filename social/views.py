55225b716dabffb1618ed3f6b1f12781eaa7bf0b
prosper-20 <edwardprosper001@gmail.com>
2022-07-28 21:56:28 +0100

    Add-ons

--- social/views.py
+++ social/views.py
@@ -2,7 +2,7 @@ import imp
 from django.shortcuts import render
 from django.views import View
 from .models import Post
-from .forms import PostForm
+from .forms import PostForm, CommentForm
 
 # Create your views here.
 
@@ -38,9 +38,11 @@ class PostListView(View):
 class PostDetailView(View):
     def get(self, request, pk, *args, **kwargs):
         post = Post.objects.get(pk=pk)
+        form = CommentForm()
 
         context = {
-            "post": post
+            "post": post,
+            "form": form,
         }
         return render(request, 'social/post_detail.html', context)
         
