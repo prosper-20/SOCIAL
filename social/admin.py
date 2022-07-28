7813d95a959607fcadb39883ec444bce9eb24989
prosper-20 <edwardprosper001@gmail.com>
2022-07-28 16:29:58 +0100

    updates

--- social/admin.py
+++ social/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Post
 
 # Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ["created_on", "author"]
+
+admin.site.register(PostAdmin, Post)
