0092a61a3635493f944eab315ffc7628a25aa9ef
prosper-20 <edwardprosper001@gmail.com>
2022-07-28 21:35:54 +0100

    Add-ons

--- social/models.py
+++ social/models.py
@@ -10,3 +10,13 @@ class Post(models.Model):
 
     def __str__(self):
         return self.body
+
+
+class Comment(models.Model):
+    comment = models.TextField()
+    created_on = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey('Post', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.comment} - {self.author}"
