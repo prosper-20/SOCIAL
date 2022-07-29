1fa427defc6d066299e9bbdd25a11d2a87478865
prosper-20 <edwardprosper001@gmail.com>
2022-07-29 18:28:30 +0100

    updates

--- social/urls.py
+++ social/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path("", PostListView.as_view(), name="post-list"),
     path("post/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
     path("post/edit/<int:pk>/", PostEditView.as_view(), name="post-edit"),
-    path("post/delete/<int:pk>", PostDeleteView, name="post-delete"),
+    path("post/delete/<int:pk>", PostDeleteView.as_view(), name="post-delete"),
 ]
