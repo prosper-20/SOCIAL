16e571c229e08e5b240942ce574f9ecca66f7a82
prosper <edwardprosper001@gmail.com>
2022-08-16 19:58:45 -0700

    Initial

--- PROJECT/settings.py
+++ PROJECT/settings.py
@@ -146,6 +146,9 @@ MEDIA_URL = "/media/"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
 
 CRISPY_TEMPLATE_PACK = "bootstrap4"
 
