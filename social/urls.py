7d4971d7c97f338b230cf69db8143b0021abcbca
prosper <edwardprosper001@gmail.com>
2022-08-18 18:17:55 -0700

    Mailing

--- social/urls.py
+++ social/urls.py
@@ -11,10 +11,11 @@ urlpatterns = [
     path("post/<int:post_pk>/comment/delete/<int:pk>/", CommentDeleteView.as_view(), name="comment-delete"),
     path('profile/edit/<int:pk>/', ProfileEditView.as_view(), name="profile-edit"),
     path("profile/<int:pk>", ProfileView.as_view(), name='profile'),
+    path("profile/,int:pk>/followers/", ListFollowers.as_view(), name="list-followers"),
     path("profile/<int:pk>/followers/add", AddFollower.as_view(), name="add-follower"),
     path("profile/<int:pk>/followers/remove", RemoveFollower.as_view(), name="remove-follower"),
     path("post/<int:pk>/like", AddLike.as_view(), name="like"),
     path('post/<int:pk>/dislike', DislikeView.as_view(), name="dislike"),
     path('search/', UserSearch.as_view(), name="profile-search"),
-    path('')
+    
 ]
