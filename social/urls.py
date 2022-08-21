71ba1a46c9c621a7c17e8e4ee83d5f9acd8b4b5e
prosper <edwardprosper001@gmail.com>
2022-08-20 22:51:07 -0700

    updates

--- social/urls.py
+++ social/urls.py
@@ -1,7 +1,24 @@
 from django.urls import path
 
 from social.models import Post
-from .views import PostListView, PostDetailView, PostEditView, PostDeleteView, CommentDeleteView, ProfileView, ProfileEditView, AddFollower, RemoveFollower, DislikeView, AddLike, UserSearch, ListFollowers, AddCommentLike, AddCommentDisLike
+from .views import ( 
+    PostListView, 
+    PostDetailView, 
+    PostEditView, 
+    PostDeleteView, 
+    CommentDeleteView, 
+    ProfileView, 
+    ProfileEditView, 
+    AddFollower, 
+    RemoveFollower, 
+    DislikeView, 
+    AddLike, 
+    UserSearch, 
+    ListFollowers, 
+    AddCommentLike, 
+    AddCommentDisLike
+
+)
 
 urlpatterns = [
     path("", PostListView.as_view(), name="post-list"),
@@ -9,6 +26,8 @@ urlpatterns = [
     path("post/edit/<int:pk>/", PostEditView.as_view(), name="post-edit"),
     path("post/delete/<int:pk>", PostDeleteView.as_view(), name="post-delete"),
     path("post/<int:post_pk>/comment/delete/<int:pk>/", CommentDeleteView.as_view(), name="comment-delete"),
+    path('post/<int:post_pk>/comment/<int:pk>/like', AddCommentLike.as_view(), name="comment-like"),
+    path("post/<int:pk>/comment/<int;pk./dislike", AddCommentDisLike.as_view(), name="comment-dislike"),
     path('profile/edit/<int:pk>/', ProfileEditView.as_view(), name="profile-edit"),
     path("profile/<int:pk>", ProfileView.as_view(), name='profile'),
     path("profile/<int:pk>/followers/", ListFollowers.as_view(), name="list-followers"),
