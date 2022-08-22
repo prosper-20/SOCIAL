ed187b1e7f1271b4a080c4323fd288e968ce4b1b
prosper <edwardprosper001@gmail.com>
2022-08-22 14:46:34 -0700

    social

--- static/js/social.js
+++ static/js/social.js
@@ -1,3 +1,10 @@
 function commentReplyToggle(parent_id) {
-    console.log(parent_id)
+    const row = document.getElementById(parent_id);
+
+    if (row.classList.contains("d-none")) {
+        row.classList.remove("d-none");
+    } else {
+        row.classList.add("d-none");
+    }
+
 }
